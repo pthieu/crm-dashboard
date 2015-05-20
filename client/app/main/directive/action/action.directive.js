a91bab86fe5c9035fcbb6c0cb8569da14f2f23cb
Phong Thieu <pthieu@Phongs-MacBook-Air.local>
2015-05-20 02:46:00 -0400

    styling changes and making the edits click instead of hover

--- client/app/main/directive/action/action.directive.js
+++ client/app/main/directive/action/action.directive.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('crmDashboardApp')
-  .directive('actionDirective', function ($http, $interval, $timeout, RecursionHelper) {
+  .directive('actionDirective', function($http, $interval, $timeout, RecursionHelper) {
     return {
       scope: {
         actionId: '=', // html:action-node, js:actionNode
@@ -10,14 +10,20 @@ angular.module('crmDashboardApp')
       templateUrl: 'app/main/directive/action/action.directive.html',
       replace: true,
       restrict: 'E',
-      compile: function (element) {
-        return RecursionHelper.compile(element, function(scope, iElement, iAttrs, controller, transcludeFn){
-          scope.deleteAction = function (_action) {
+      compile: function(element) {
+        return RecursionHelper.compile(element, function(scope, iElement, iAttrs, controller, transcludeFn) {
+          scope.deleteAction = function(e, _action) {
+            e.stopPropagation();
+            
             var id = _action._id;
             $http.delete('/api/actions', {
-                data: {'id':id},
-                headers: {"Content-Type": "application/json;charset=utf-8"} // we need to do this if we want to send params, otherwise we need to do traditional REST in URL
-              });
+              data: {
+                'id': id
+              },
+              headers: {
+                "Content-Type": "application/json;charset=utf-8"
+              } // we need to do this if we want to send params, otherwise we need to do traditional REST in URL
+            });
           };
           // GETS for API -- TODO: do we need to finish getAction? we already have entire list of actions
           // scope.getAction = function (_id) {
@@ -26,17 +32,89 @@ angular.module('crmDashboardApp')
           //   });
           // };
           // Find for already called action list
-          scope.findAction = function (_id, _list) {
-            scope.actionNode = _.findWhere(_list, {_id:_id})
+          scope.findAction = function(_id, _list) {
+            scope.actionNode = _.findWhere(_list, {
+              _id: _id
+            })
           };
           scope.findAction(scope.actionId, scope.actionList);
 
           // Declare and initialize self-invoking function
-          (function calculateTimeSince(){
+          (function calculateTimeSince() {
             scope.fromNow = moment(scope.actionNode.content).fromNow(true); // moment.js will handle output format depending on length of time passed
             // scope.fromNow = moment().diff(scope.actionNode.content, 'days') // will always output in days
             $timeout(calculateTimeSince, 1000); // we use $timeout because it syncs the view with the model and updates with $apply. setTimeout will not work here.
           })();
+
+          scope.newChildAction = function(e, _action) {
+            e.stopPropagation();
+          };
+
+
+
+
+          // BEGIN datepicker logic
+          scope.today = function() {
+            scope.dt = new Date();
+          };
+          scope.today();
+
+          scope.clear = function() {
+            scope.dt = null;
+          };
+
+          // Disable weekend selection
+          scope.disabled = function(date, mode) {
+            return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 6));
+          };
+
+          scope.toggleMin = function() {
+            scope.minDate = scope.minDate ? null : new Date();
+          };
+          scope.toggleMin();
+
+          scope.open = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+
+            scope.opened = true;
+          };
+
+          scope.dateOptions = {
+            formatYear: 'yy',
+            startingDay: 1
+          };
+
+          scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+          scope.format = scope.formats[0];
+
+          var tomorrow = new Date();
+          tomorrow.setDate(tomorrow.getDate() + 1);
+          var afterTomorrow = new Date();
+          afterTomorrow.setDate(tomorrow.getDate() + 2);
+          scope.events = [{
+            date: tomorrow,
+            status: 'full'
+          }, {
+            date: afterTomorrow,
+            status: 'partially'
+          }];
+
+          scope.getDayClass = function(date, mode) {
+            if (mode === 'day') {
+              var dayToCheck = new Date(date).setHours(0, 0, 0, 0);
+
+              for (var i = 0; i < scope.events.length; i++) {
+                var currentDay = new Date(scope.events[i].date).setHours(0, 0, 0, 0);
+
+                if (dayToCheck === currentDay) {
+                  return scope.events[i].status;
+                }
+              }
+            }
+            return '';
+          };
+          // END datepicker logic
         });
       }
     };
